c98a4fc03d011b37738b7f07c40c0ba0ec210767
yousseffathiMBA <yousseffathin1@gmail.com>
2025-05-26 13:33:15 +0100

    chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -546,7 +546,7 @@
             responseTimeText: '',
             poweredBy: {
                 text: 'Powered by FathiShop',
-                link: 'https://n8n.partnerlinks.io/fabimarkl'
+                link: 'https://fathishop.store'
             }
         },
         style: {
@@ -606,7 +606,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
                 </svg>
-                Start chatting
+                اضغط هنا للتحدث
             </button>
             <p class="chat-response-time">${settings.branding.responseTimeText}</p>
         </div>
