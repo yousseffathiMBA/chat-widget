e982c901620066d7703d56cc1094529ff4786c56
yousseffathiMBA <yousseffathin1@gmail.com>
2025-06-04 09:14:58 +0100

    chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -330,8 +330,8 @@
 
         .chat-assist-widget .chat-launcher {
             position: fixed;
-            bottom: 40px;
-            height: 30px;
+            bottom: 36px;
+            height: 40px;
             border-radius: var(--chat-radius-full);
             background: linear-gradient(135deg, var(--chat-color-primary) 0%, var(--chat-color-secondary) 100%);
             color: white;
